c81902fed486124ea6a1c45f6cca6f3d11298f7a
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-11-06 23:06:10 +0200

    added header & footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,65 @@
+import { NavLink } from "react-router-dom";
+import Button from "../Button/Button";
 import css from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={css.container}>
-      <p>Footer</p>
+      <ul className={css.footerList}>
+        <li>
+          <h3 className={css.title}>Інформація</h3>
+          <ul className={css.linksList}>
+            <li className={css.link}>
+              <NavLink to="/about">Про нас</NavLink>
+            </li>
+            <li className={css.link}>
+              <NavLink to="/delivery">Доставка і оплата</NavLink>
+            </li>
+            <li className={css.link}>
+              <NavLink to="/guarantee">Гарантія</NavLink>
+            </li>
+            <li className={css.link}>
+              <NavLink to="/">Бренди</NavLink>
+            </li>
+            <li className={css.link}>
+              <NavLink to="/">Акції</NavLink>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h3 className={css.title}>
+            <NavLink to="/">Служба підтримки</NavLink>
+          </h3>
+          <ul className={css.linksList}>
+            <li className={css.link}>
+              <NavLink to="/">Контакти</NavLink>
+            </li>
+            <li className={css.link}>
+              <NavLink to="/">Повернення товару</NavLink>
+            </li>
+            <li className={css.link}>
+              <NavLink to="/">Політика безпеки</NavLink>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h3 className={css.title}>Слідкуйте за акціями та новинами</h3>
+          <form>
+            <label>
+              <input
+                required
+                className={css.input}
+                type="email"
+                name="email"
+                placeholder="Введіть ваш Email"
+              />
+            </label>
+            <div className={css.btnWrap}>
+              <Button title={"Підписатися"}></Button>
+            </div>
+          </form>
+        </li>
+      </ul>
     </footer>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,94 +1,37 @@
 import { NavLink } from "react-router-dom";
 import css from "./Header.module.css";
-import Select from "react-select";
 import Button from "../Button/Button";
-
-const selectOptions = [
-  { value: "Clothes", label: "Одяг та взуття" },
-  { value: "Toys", label: "Іграшки" },
-  { value: "Room", label: "Дитяча кімната" },
-  { value: "Strollers", label: "Коляски" },
-  { value: "Seats", label: "Автокрісла" },
-  { value: "Feeding", label: "Все для годування" },
-  { value: "Care", label: "Догляд, гігієна та купання" },
-  { value: "Transport", label: "Дитячий транспорт" },
-];
-
-const selectStyles = {
-  control: () => ({
-    display: "flex",
-    paddingRight: "10px",
-    color: "#000",
-    fontSize: "18px",
-    fontWeight: "500",
-    lineHeight: "1.11",
-  }),
-  option: (styles, { isFocused }) => {
-    return {
-      ...styles,
-      backgroundColor: "transparent",
-      color: isFocused ? "#121417" : "rgba(18, 20, 23, 0.20)",
-      fontSize: "16px",
-      fontWeight: "500",
-      lineHeight: "1.25",
-      cursor: "pointer",
-      paddingLeft: "0",
-      paddingRight: "0",
-      paddingTop: "4px",
-      paddingBottom: "4px",
-    };
-  },
-  placeholder: (styles) => {
-    return {
-      ...styles,
-      color: "#121417",
-    };
-  },
-  dropdownIndicator: (styles, state) => ({
-    ...styles,
-    svg: {
-      fill: "#121417",
-    },
-    cursor: "pointer",
-    transition: "transform 250ms linear",
-    transform: state.isFocused ? "rotate(180deg)" : null,
-  }),
-  menu: (styles) => ({
-    ...styles,
-    borderRadius: "10px",
-    paddingLeft: "18px",
-    paddingTop: "10px",
-    paddingBottom: "18px",
-    paddingRight: "8px",
-  }),
-  menuList: (styles) => ({
-    ...styles,
-    "::-webkit-scrollbar": {
-      width: "8px",
-      height: "0px",
-    },
-    "::-webkit-scrollbar-thumb": {
-      background: "rgba(18, 20, 23, 0.05)",
-      borderRadius: "10px",
-    },
-  }),
-};
+import Modal from "../Modal/Modal";
+import { useState } from "react";
 
 const Header = () => {
+  const [isModalOpen, setIsOpenModal] = useState(false);
+
+  const handleCategoryModalOpen = () => {
+    setIsOpenModal(!isModalOpen);
+  };
+
   return (
     <header className={css.container}>
-      <NavLink to="/">
-        <span className={css.logo}>DitOk</span>
-      </NavLink>
-      <div className={css.searchName}>
-        <Select
-          styles={selectStyles}
-          options={selectOptions}
-          className={css.selectInput}
-          placeholder="Пошук за товарами"
-        />
-        <div className={css.btnWrap}>
-          <Button title={"Знайти"} />
+      <div className={css.logoWrapper}>
+        <NavLink className={css.logo} to="/">
+          DitOk
+        </NavLink>
+        <div className={css.searchWrap}>
+          <label>
+            <input
+              type="text"
+              name="filter"
+              placeholder="Пошук за товарами"
+              className={css.input}
+            />
+          </label>
+          <button className={css.categoryBtn} onClick={handleCategoryModalOpen}>
+            Категорія{" "}
+          </button>
+          <div className={css.btnWrap}>
+            <Button title={"Знайти"} />
+          </div>
         </div>
       </div>
       <nav className={css.navigation}>
@@ -107,6 +50,7 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      {isModalOpen && <Modal />}
     </header>
   );
 };
