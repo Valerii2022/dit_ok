c5aa15ab17e2680d5f2305463dd8bfe71f64286a
Valerii Pometun <vmpometun@gmail.com>
2023-11-07 11:25:50 +0200

    adde log in & sign up pages styles

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -5,30 +5,32 @@ import Button from "../../components/Button/Button";
 const LogIn = () => {
   return (
     <div className={css.container}>
-      <img src="public/log-in.png" alt="Child on play" />
-      <div className={css.formWrap}>
+      <img src="../../../public/log-in.png" alt="Child on play" />
+      <div>
         <h2 className={css.title}>Увійти</h2>
-        <form>
-          <label className={css.label}>
-            Email
-            <input type="email" name="emailLogIn" className={css.input} />
-          </label>
-          <label className={css.label}>
-            Пароль
-            <input type="text" name="passLogIn" className={css.input} />
-          </label>
-        </form>
-        <div>
-          <div className={css.logInLink}>
-            <Button title={"Увійти"} fontSize={"28"} />
+        <form className={css.form}>
+          <div className={css.inputWrapper}>
+            <label className={css.label}>
+              Email
+              <input type="email" name="emailLogIn" className={css.input} />
+            </label>
+            <label className={css.label}>
+              Пароль
+              <input type="text" name="passLogIn" className={css.input} />
+            </label>
           </div>
-          <p className={css.linkWrap}>
-            Вперше тут?
-            <NavLink className={css.navlink} to="/signup">
-              Зареєструватися
-            </NavLink>
-          </p>
-        </div>
+          <div>
+            <div className={css.logInLink}>
+              <Button title={"Увійти"} fontSize={"28"} />
+            </div>
+            <p className={css.linkWrap}>
+              Вперше тут?
+              <NavLink className={css.navlink} to="/signup">
+                Зареєструватися
+              </NavLink>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -5,15 +5,53 @@ import Button from "../../components/Button/Button";
 const SignUp = () => {
   return (
     <div className={css.container}>
-      <div className={css.logInLink}>
-        <Button title={"Зареєструватися"} fontSize={"28"} />
+      <img src="../../../public/sign-up.jpg" alt="Child on play" />
+      <div>
+        <h2 className={css.title}>Зареєструватися</h2>
+        <form className={css.form}>
+          <div className={css.inputWrapper}>
+            <label className={css.label}>
+              Ім&#96;я
+              <input type="text" name="signUpName" className={css.input} />
+            </label>
+            <label className={css.label}>
+              Прізвище
+              <input type="text" name="signUpSurname" className={css.input} />
+            </label>
+            <label className={css.label}>
+              Номер телефону
+              <input type="tel" name="signUpPhone" className={css.input} />
+            </label>
+            <label className={css.label}>
+              Email
+              <input type="email" name="signUpEmail" className={css.input} />
+            </label>
+            <label className={css.label}>
+              Пароль
+              <input type="password" name="signUpPass" className={css.input} />
+            </label>
+            <label className={css.label}>
+              Підтвердіть пароль
+              <input
+                type="password"
+                name="signUpPassConfirm"
+                className={css.input}
+              />
+            </label>
+          </div>
+          <div>
+            <div className={css.logInLink}>
+              <Button title={"Зареєструватися"} fontSize={"28"} />
+            </div>
+            <p className={css.linkWrap}>
+              Вже є акаунт?
+              <NavLink className={css.navlink} to="/login">
+                Увійти
+              </NavLink>
+            </p>
+          </div>
+        </form>
       </div>
-      <p className={css.linkWrap}>
-        Вже є акаунт?
-        <NavLink className={css.navlink} to="/login">
-          Увійти
-        </NavLink>
-      </p>
     </div>
   );
 };
