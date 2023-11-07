a84a16ac190717620c05d23edbaec92c31add3e3
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-11-07 19:14:47 +0200

    test routes basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/dit_ok">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
