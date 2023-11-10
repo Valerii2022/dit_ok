1524f73ddc9a8ab37c22938118c75575afe5266e
Valerii Pometun <vmpometun@gmail.com>
2023-11-10 08:53:41 +0200

    added card template / feractoring home page

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,83 @@
+import { useDispatch, useSelector } from "react-redux";
+import css from "./Card.module.css";
+import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
+import { getFavourites } from "../../redux/selectors";
+import heartSvg from "../../images/heart.svg";
+import favouriteSvg from "../../images/markedHeart.svg";
+import { addToFavourites, removeFromFavourites } from "../../redux/usersSlice";
+
+const Card = (elem) => {
+  const { cardElement, openModal } = elem;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuth = false;
+  const favourites = useSelector(getFavourites);
+
+  const description = [
+    cardElement.description,
+    cardElement.color,
+    cardElement.brand,
+    cardElement.material,
+  ].join(". ");
+
+  return (
+    <li className={css.card}>
+      <div className={css.imageWrap}>
+        <img src={cardElement.src} alt={cardElement.alt} />
+        {favourites.includes(cardElement.id) ? (
+          <div
+            id={cardElement.id}
+            className={css.addToFavourites}
+            onClick={(e) => dispatch(removeFromFavourites(e.currentTarget.id))}
+          >
+            <img src={favouriteSvg} alt="Heart" width={30} />
+          </div>
+        ) : (
+          <div
+            id={cardElement.id}
+            className={css.addToFavourites}
+            onClick={(e) => dispatch(addToFavourites(e.currentTarget.id))}
+          >
+            <img src={heartSvg} alt="Heart" width={30} />
+          </div>
+        )}
+      </div>
+      <p className={css.description}>
+        {description.length < 120
+          ? `${description}.`
+          : `${description.slice(0, 110)}...`}
+      </p>
+      {cardElement.sale ? (
+        <p>
+          <span>{cardElement.sale} грн</span>
+          <span className={css.salePrice}>{cardElement.price} грн</span>
+        </p>
+      ) : (
+        <p>{cardElement.price} грн</p>
+      )}
+      <div className={css.cardBottom}>
+        <div
+          className={css.btnWrap}
+          onClick={() => {
+            {
+              isAuth ? navigate("/order") : openModal(true);
+            }
+          }}
+        >
+          <Button title={"Купити"} />
+        </div>
+        <button
+          className={css.moreBtn}
+          onClick={() => {
+            navigate("/item");
+          }}
+        >
+          Докладніше
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default Card;
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,8 +1,6 @@
 import css from "./Home.module.css";
 // import Carousel from "better-react-carousel";
 import Button from "../../components/Button/Button.jsx";
-import heartSvg from "../../images/heart.svg";
-import favouriteSvg from "../../images/markedHeart.svg";
 import { useNavigate } from "react-router-dom";
 import closeIcon from "../../images/close.svg";
 import { useEffect, useState } from "react";
@@ -10,23 +8,21 @@ import Modal from "../../components/Modal/Modal";
 import reviews from "../../redux/feedback.js";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { getFavourites, getItems } from "../../redux/selectors.js";
-import { addToFavourites, removeFromFavourites } from "../../redux/usersSlice";
+import { getItems } from "../../redux/selectors.js";
 import { fetchAdverts } from "../../redux/operations";
 import AboutUs from "../../components/AboutUs/AboutUs";
+import Card from "../../components/Card/Card.jsx";
 
 const Home = () => {
   const [unregisterModal, setUnregisterModal] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isAuth = false;
   const { adverts, isLoading } = useSelector(getItems);
 
   useEffect(() => {
     dispatch(fetchAdverts());
   }, [dispatch]);
 
-  const favourites = useSelector(getFavourites);
   const discountAdverts = adverts.filter((el) => el.sale);
 
   const handleUnregisterModalOpen = () => {
@@ -40,257 +36,29 @@ const Home = () => {
           <h2 className={css.title}>Нові надходження</h2>
           <ul className={css.caruselWrap}>
             {isLoading && <p>Loading...</p>}
-            {adverts.map((elem) => {
-              const description = [
-                elem.description,
-                elem.color,
-                elem.brand,
-                elem.material,
-              ].join(". ");
+            {adverts.map((element) => {
               return (
-                <li key={elem.id} className={css.card}>
-                  <div className={css.imageWrap}>
-                    <img src={elem.src} alt={elem.alt} />
-                    {favourites.includes(elem.id) ? (
-                      <div
-                        id={elem.id}
-                        className={css.addToFavourites}
-                        onClick={(e) =>
-                          dispatch(removeFromFavourites(e.currentTarget.id))
-                        }
-                      >
-                        <img src={favouriteSvg} alt="Heart" width={30} />
-                      </div>
-                    ) : (
-                      <div
-                        id={elem.id}
-                        className={css.addToFavourites}
-                        onClick={(e) =>
-                          dispatch(addToFavourites(e.currentTarget.id))
-                        }
-                      >
-                        <img src={heartSvg} alt="Heart" width={30} />
-                      </div>
-                    )}
-                  </div>
-                  <p className={css.description}>
-                    {description.length < 120
-                      ? `${description}.`
-                      : `${description.slice(0, 110)}...`}
-                  </p>
-                  {elem.sale ? (
-                    <p>
-                      <span>{elem.sale} грн</span>
-                      <span className={css.salePrice}>{elem.price} грн</span>
-                    </p>
-                  ) : (
-                    <p>{elem.price} грн</p>
-                  )}
-                  <div className={css.cardBottom}>
-                    <div
-                      className={css.btnWrap}
-                      onClick={() => {
-                        {
-                          isAuth
-                            ? navigate("/order")
-                            : setUnregisterModal(true);
-                        }
-                      }}
-                    >
-                      <Button title={"Купити"} />
-                    </div>
-                    <button
-                      className={css.moreBtn}
-                      onClick={() => {
-                        navigate("/item");
-                      }}
-                    >
-                      Докладніше
-                    </button>
-                  </div>
-                </li>
+                <Card
+                  key={element.id}
+                  cardElement={element}
+                  openModal={setUnregisterModal}
+                />
               );
             })}
           </ul>
-          {/* <Carousel cols={4} rows={1} gap={20} loop>
-          <Carousel.Item>
-            <div className={css.card}>
-              <div className={css.imageWrap}>
-                <img src="/truck.jpg" alt="Truck" width={280} height={280} />
-                <div className={css.addToFavourites}></div>
-              </div>
-              <p>
-                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
-                Поліестер 30%, бавовна 70% Flamingo
-              </p>
-              <p>1440 грн</p>
-              <div className={css.cardBottom}>
-                <div className={css.btnWrap}>
-                  <Button title={"Купити"} />
-                </div>
-                <button className={css.moreBtn}>Докладніше</button>
-              </div>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className={css.card}>
-              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
-              <p>
-                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
-                Поліестер 30%, бавовна 70% Flamingo
-              </p>
-              <p>1440 грн</p>
-              <div className={css.cardBottom}>
-                <div className={css.btnWrap}>
-                  <Button title={"Купити"} />
-                </div>
-                <button className={css.moreBtn}>Докладніше</button>
-              </div>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className={css.card}>
-              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
-              <p>
-                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
-                Поліестер 30%, бавовна 70% Flamingo
-              </p>
-              <p>1440 грн</p>
-              <div className={css.cardBottom}>
-                <div className={css.btnWrap}>
-                  <Button title={"Купити"} />
-                </div>
-                <button className={css.moreBtn}>Докладніше</button>
-              </div>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className={css.card}>
-              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
-              <p>
-                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
-                Поліестер 30%, бавовна 70% Flamingo
-              </p>
-              <p>1440 грн</p>
-              <div className={css.cardBottom}>
-                <div className={css.btnWrap}>
-                  <Button title={"Купити"} />
-                </div>
-                <button className={css.moreBtn}>Докладніше</button>
-              </div>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className={css.card}>
-              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
-              <p>
-                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
-                Поліестер 30%, бавовна 70% Flamingo
-              </p>
-              <p>1440 грн</p>
-              <div className={css.cardBottom}>
-                <div className={css.btnWrap}>
-                  <Button title={"Купити"} />
-                </div>
-                <button className={css.moreBtn}>Докладніше</button>
-              </div>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className={css.card}>
-              <div className={css.imageWrap}>
-                <img src="/truck.jpg" alt="Truck" width={280} height={280} />
-                <div className={css.addToFavourites}></div>
-              </div>
-              <p>
-                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
-                Поліестер 30%, бавовна 70% Flamingo
-              </p>
-              <p>1440 грн</p>
-              <div className={css.cardBottom}>
-                <div className={css.btnWrap}>
-                  <Button title={"Купити"} />
-                </div>
-                <button className={css.moreBtn}>Докладніше</button>
-              </div>
-            </div>
-          </Carousel.Item>
-        </Carousel> */}
         </section>
       </div>
       <div className={css.container}>
         <section className={css.discontSection}>
           <h2 className={css.title}>Акції</h2>
           <ul className={css.caruselWrap}>
-            {discountAdverts.map((elem) => {
-              const description = [
-                elem.description,
-                elem.color,
-                elem.brand,
-                elem.material,
-              ].join(". ");
+            {discountAdverts.map((element) => {
               return (
-                <li key={elem.id} className={css.card}>
-                  <div className={css.imageWrap}>
-                    <img src={elem.src} alt="Truck" width={280} height={280} />
-                    {favourites.includes(elem.id) ? (
-                      <div
-                        id={elem.id}
-                        className={css.addToFavourites}
-                        onClick={(e) =>
-                          dispatch(removeFromFavourites(e.currentTarget.id))
-                        }
-                      >
-                        <img src={favouriteSvg} alt="Heart" width={30} />
-                      </div>
-                    ) : (
-                      <div
-                        id={elem.id}
-                        className={css.addToFavourites}
-                        onClick={(e) =>
-                          dispatch(addToFavourites(e.currentTarget.id))
-                        }
-                      >
-                        <img src={heartSvg} alt="Heart" width={30} />
-                      </div>
-                    )}
-                  </div>
-                  <p>
-                    {description.length < 120
-                      ? `${description}.`
-                      : `${description.slice(0, 110)}...`}
-                  </p>
-                  {elem.sale ? (
-                    <p>
-                      <span>{elem.sale} грн</span>
-                      <span className={css.salePrice}>{elem.price} грн</span>
-                    </p>
-                  ) : (
-                    <p>{elem.price} грн</p>
-                  )}
-                  <div className={css.cardBottom}>
-                    <div
-                      className={css.btnWrap}
-                      onClick={() => {
-                        {
-                          isAuth
-                            ? navigate("/order")
-                            : setUnregisterModal(true);
-                        }
-                      }}
-                    >
-                      <Button title={"Купити"} />
-                    </div>
-                    <button
-                      className={css.moreBtn}
-                      onClick={() => {
-                        navigate("/item");
-                      }}
-                    >
-                      Докладніше
-                    </button>
-                  </div>
-                </li>
+                <Card
+                  key={element.id}
+                  cardElement={element}
+                  openModal={setUnregisterModal}
+                />
               );
             })}
           </ul>
@@ -347,3 +115,110 @@ const Home = () => {
 };
 
 export default Home;
+
+{
+  /* <Carousel cols={4} rows={1} gap={20} loop>
+          <Carousel.Item>
+            <div className={css.card}>
+              <div className={css.imageWrap}>
+                <img src="/truck.jpg" alt="Truck" width={280} height={280} />
+                <div className={css.addToFavourites}></div>
+              </div>
+              <p>
+                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
+                Поліестер 30%, бавовна 70% Flamingo
+              </p>
+              <p>1440 грн</p>
+              <div className={css.cardBottom}>
+                <div className={css.btnWrap}>
+                  <Button title={"Купити"} />
+                </div>
+                <button className={css.moreBtn}>Докладніше</button>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={css.card}>
+              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
+              <p>
+                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
+                Поліестер 30%, бавовна 70% Flamingo
+              </p>
+              <p>1440 грн</p>
+              <div className={css.cardBottom}>
+                <div className={css.btnWrap}>
+                  <Button title={"Купити"} />
+                </div>
+                <button className={css.moreBtn}>Докладніше</button>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={css.card}>
+              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
+              <p>
+                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
+                Поліестер 30%, бавовна 70% Flamingo
+              </p>
+              <p>1440 грн</p>
+              <div className={css.cardBottom}>
+                <div className={css.btnWrap}>
+                  <Button title={"Купити"} />
+                </div>
+                <button className={css.moreBtn}>Докладніше</button>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={css.card}>
+              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
+              <p>
+                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
+                Поліестер 30%, бавовна 70% Flamingo
+              </p>
+              <p>1440 грн</p>
+              <div className={css.cardBottom}>
+                <div className={css.btnWrap}>
+                  <Button title={"Купити"} />
+                </div>
+                <button className={css.moreBtn}>Докладніше</button>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={css.card}>
+              <img src="/truck.jpg" alt="Truck" width={280} height={280} />
+              <p>
+                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
+                Поліестер 30%, бавовна 70% Flamingo
+              </p>
+              <p>1440 грн</p>
+              <div className={css.cardBottom}>
+                <div className={css.btnWrap}>
+                  <Button title={"Купити"} />
+                </div>
+                <button className={css.moreBtn}>Докладніше</button>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={css.card}>
+              <div className={css.imageWrap}>
+                <img src="/truck.jpg" alt="Truck" width={280} height={280} />
+                <div className={css.addToFavourites}></div>
+              </div>
+              <p>
+                Утепленний комплекс (штанці і кофтинка) для хлопчика. Сірий.
+                Поліестер 30%, бавовна 70% Flamingo
+              </p>
+              <p>1440 грн</p>
+              <div className={css.cardBottom}>
+                <div className={css.btnWrap}>
+                  <Button title={"Купити"} />
+                </div>
+                <button className={css.moreBtn}>Докладніше</button>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel> */
+}
